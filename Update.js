0f6e926ab36aed57f31efd3cfe29977a697a3c19
tradewithjuttsab-cell <tradewithjuttsab@gmail.com>
2026-09-17 16:26:55 +0500

    Update Update.js

--- Update.js
+++ Update.js
@@ -1,11 +1,1 @@
 
-(function() {
-  document.querySelectorAll('*').forEach(el => {
-    if (el.children.length === 0 && el.innerText) {
-      let txt = el.innerText.trim();
-      if (txt.includes('DEMO')) {
-        el.innerText = 'LIVE ACCOUNT';
-      }
-    }
-  });
-})();
